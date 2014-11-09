bb7d99547fbed1920487c07677d68cccf14a0115
Benjamin Falk <benjamin.falk@yahoo.com>
2014-11-09 10:58:58 -0600

    Bumping the documentation coverage up

--- lib/crystal_forge/document_parser.rb
+++ lib/crystal_forge/document_parser.rb
@@ -7,24 +7,26 @@ module CrystalForge
   # Responsible for taking an api string and providing
   # information about it
   class DocumentParser
-    # == Parameters
-    # * api : an api blueprint or raml string
+    ##
+    # Initialize document parser with an api string
+    # @param [String] api
     def initialize(api)
       @ast = AST.parse(api)
       @resource_opts = {}
       after_initialize
     end
 
-    # == Overview
+    ##
     # Array of resources generated with the resource options
     # initialized by the DocumentParser
+    # @return [Array<Resource>]
     def resources
       ast.resources.map { |r| Resource.new(r, resource_opts) }
     end
 
     private
 
-    # == Overview
+    ##
     # Can be called by sub classes to extend functionality
     # and not have to couple into calling initialize
     def after_initialize
--- lib/crystal_forge/formats/raml.rb
+++ lib/crystal_forge/formats/raml.rb
@@ -2,17 +2,29 @@ require 'raml-rb'
 
 module CrystalForge
   module Formats
+    ##
+    # RAML ast that can convert a raml based document into a similiar structure
+    # that is used with Crystal Forge
     class RAML < DocumentParser::AST
       HEADER_REGEX = /\A\#%RAML 0.8/
 
+      ##
+      # Determines if the given document could be used
+      # @param [String] document api string to analyze
+      # @param [Hash] _opts unused options
+      # @return [true, false]
       def self.understands?(document, _opts = {})
         document.scan(HEADER_REGEX).size == 1
       end
 
+      ##
+      # @param [String] document
       def initialize(document)
         @raml = Raml::Parser.parse(document)
       end
 
+      ##
+      # @return [Array<RawResource>]
       def resources
         non_empty_resources.map { |r| RawResource.new(r) }
       end
@@ -26,16 +38,28 @@ module CrystalForge
         end
       end
 
+      ##
+      # Wrapper for resources from the raml parser that follow the
+      # same signiture as an API blueprint resource
       class RawResource
+        ##
+        # @param [Raml::Resource] node
         def initialize(node)
           @node = node
           node.methods.each { |m| m.method.upcase! }
         end
 
+        ##
+        # @return [Array<RAML::RawAction>]
         def actions
           node.methods.map { |method| RawAction.new(method) }
         end
 
+        ##
+        # @example
+        #   "/widgets/{id}"
+        #
+        # @return [String]
         def uri_template
           uri_partials.join
         end
@@ -55,15 +79,27 @@ module CrystalForge
         attr_reader :node
       end
 
+      ##
+      # Wrapper object that gives each raml method the same duct type
+      # as an api blueprint action
       class RawAction
+        ##
+        # @param [Raml::Method] action
         def initialize(action)
           @action = action
         end
 
+        ##
+        # @return [Array<RawExample>]
         def examples
           action.responses.map { |response| RawExample.new(response) }
         end
 
+        ##
+        # @example
+        #   "GET"
+        #
+        # @return [String] representing http verb
         def method
           action.method.upcase
         end
@@ -73,11 +109,18 @@ module CrystalForge
         attr_reader :action
       end
 
+      ##
+      # Wraps a given raml response to have the same duct type
+      # as an api blueprint example
       class RawExample
+        ##
+        # @param [Raml::Response] response
         def initialize(response)
           @response = response
         end
 
+        ##
+        # @return [Array<RAML::RawResponse>]
         def responses
           return [RawResponse.new(nil, response)] if response.bodies.empty?
           response.bodies.map { |body| RawResponse.new(body, response) }
@@ -88,25 +131,41 @@ module CrystalForge
         attr_reader :response
       end
 
+      ##
+      # response that is similiar to an API blueprint response
       class RawResponse
+        ##
+        # @param [Raml::Body] raw_body
+        # @param [Raml::Response] response
         def initialize(raw_body, response)
           @raw_body = raw_body
           @response = response
         end
 
+        ##
+        # @return [String] status html code
         def name
           response.code.to_s
         end
 
+        ##
+        # @return [String] html body
         def body
           return '' if raw_body.nil?
           raw_body.example
         end
 
+        ##
+        # @return [RAML::RawResponse, #collection]
         def headers
           self
         end
 
+        ##
+        # @example
+        #   [{ name: 'Content-Type', value: 'text/html' }]
+        # @return [Array<Hash>] array of name, value pair hashes for
+        #   html headers
         def collection
           return [] if raw_body.nil?
           [{ name: 'Content-Type', value: raw_body.content_type }]
