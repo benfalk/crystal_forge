095844b62f69345241db9b1bbecab396448295a9
Benjamin Falk <benjamin.falk@yahoo.com>
2014-11-10 22:31:38 -0600

    Preparing for complex examples
    Interestingly enough, it appears that the APIB file may be fairly
    finicky.  The routing table did not "snap" into place like I was
    expecting it to until I had all actions looking like this:
    
    ```
      ## Widget [GET]
    ```
    
    Where as this did not work:
    
    ```
      ## [GET]
    ```

--- spec/lib/crystal_forge/routing_table_spec.rb
+++ spec/lib/crystal_forge/routing_table_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 module CrystalForge
   describe CrystalForge::RoutingTable do
     let(:example_apib) { File.read(HELLO_WORLD_APIB) }
+    let(:widget_apib)  { File.read(WIDGET_APIB) }
 
     context 'Initialized with Hello World apib' do
       let(:instance) { described_class.new(example_apib) }
@@ -16,6 +17,16 @@ module CrystalForge
       describe '#routes' do
         subject { instance.routes }
         its(:count) { is_expected.to eq(2) }
+        it { is_expected.to all be_a(DocumentParser::SimpleRoute) }
+      end
+    end
+
+    context 'Initialized with Widgets apib' do
+      let(:instance) { described_class.new(widget_apib) }
+
+      describe '#routes' do
+        subject { instance.routes }
+        its(:count) { is_expected.to eq(5) }
         it { is_expected.to all be_a(DocumentParser::SimpleRoute) }
       end
     end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,4 @@ require 'crystal_forge'
 
 HELLO_WORLD_APIB = "#{File.dirname(__FILE__)}/fixtures/apib_files/hello_world.apib"
 HELLO_WORLD_RAML = "#{File.dirname(__FILE__)}/fixtures/raml_files/hello_world.raml"
+WIDGET_APIB = "#{File.dirname(__FILE__)}/fixtures/apib_files/widgets.apib"
